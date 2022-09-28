16aaaff280598205b6a68ec17553f8cbc3f3066e
Wyzzy <ilgizn395@gmail.com>
2022-09-28 16:59:09 +0300

    second

--- Task3/task.py
+++ Task3/task.py
@@ -6,8 +6,7 @@ def f(x):
     return 1/np.cos(x)
 
 # вычисляем определенный интеграл при помощи метода quad
-res = sp.integrate.quad(f, 0.0, np.pi/4)
-print(res[0])
+print(sp.integrate.quad(f, 0.0, np.pi/4)[0])
 
 # вычисление решения, полученного при ручном счете интеграла
 print(np.log(2.0/np.sqrt(2)+1.0))
