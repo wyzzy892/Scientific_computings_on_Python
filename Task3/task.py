707726446eaa12a1a91f9d4a5274cc647947020c
Wyzzy <ilgizn395@gmail.com>
2022-10-02 19:12:13 +0300

    complete

--- Task3/task.py
+++ Task3/task.py
@@ -1,12 +1,8 @@
 import numpy as np
 import scipy as sp
 
-# определяем подыинтегральную функцию
-def f(x):
-    return 1/np.cos(x)
-
 # вычисляем определенный интеграл при помощи метода quad
-print(sp.integrate.quad(f, 0.0, np.pi/4)[0])
+print(sp.integrate.quad(lambda x: 1/np.cos(x), 0.0, np.pi/4)[0])
 
 # вычисление решения, полученного при ручном счете интеграла
 print(np.log(2.0/np.sqrt(2)+1.0))
